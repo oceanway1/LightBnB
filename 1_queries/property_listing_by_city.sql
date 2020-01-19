a0abdda1de2640bd78611fa6ab86527d3bc44a0c
oceanway1 <oceanway1@yahoo.ca>
2020-01-19 03:27:47 +0000

    add property function

--- 1_queries/property_listing_by_city.sql
+++ 1_queries/property_listing_by_city.sql
@@ -1,10 +1,3 @@
--- SELECT * 
--- FROM properties
--- JOIN property_reviews ON property_id = properties.id
--- WHERE rating >= 4
--- AND city = 'Vancouver'
--- ORDER BY cost_per_night ASC
--- LIMIT 10
 
 SELECT properties.*, avg(property_reviews.rating) as average_rating
 FROM properties
